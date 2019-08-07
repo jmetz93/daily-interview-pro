ec352984ca74994eb7cd880f3e8840a1a227467d
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-08-07 10:09:56 -0700

    Adding SortListOfNums question, unsolved

---
+++ Problems/SortListOfNums.py
@@ -0,0 +1,14 @@
+# Given a list of numbers with only 3 unique numbers (1, 2, 3), sort the list in O(n) time.
+
+# Example 1:
+# Input: [3, 3, 2, 1, 3, 2, 1]
+# Output: [1, 1, 2, 2, 3, 3, 3]
+
+# Challenge: Try sorting the list using constant space.
+
+def sortNums(nums):
+  # Fill this in.
+
+
+print sortNums([3, 3, 2, 1, 3, 2, 1])
+# [1, 1, 2, 2, 3, 3, 3]
