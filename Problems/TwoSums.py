1df2fb3ac4ee017ef133401118c1343661f1faf6
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-08-08 10:19:56 -0700

    Adding TwoSums question unsolved

---
+++ Problems/TwoSums.py
@@ -0,0 +1,13 @@
+# You are given a list of numbers, and a target number k. Return whether or not there are two numbers in the list that add up to k.
+
+# Example:
+# Given [4, 7, 1 , -3, 2] and k = 5, 
+# return true since 4 + 1 = 5.
+
+# Challenge: Try to do it in a single pass of the list.
+
+def two_sum(list, k):
+  # Fill this in.
+
+print two_sum([4,7,1,-3,2], 5)
+# True
